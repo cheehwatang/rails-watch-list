273a126e2405678998b905448b059333a381913f
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-09 23:43:17 +0800

    Adds validation to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :list
+
+  validates :content, presence: true, length: { minimum: 6 }
+  validates :rating,  presence: true, numericality: { only_integer: true, in: (0..5) }
 end
