5868a9856336ba6cb9523c05f672cf9e50fe2a5a
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-09 16:09:41 +0800

    Adds routing from list to show movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   def show
+    @list = List.find(params[:list_id])
     @movie = Movie.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'lists#index'
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
+    resources :movies, only: %i[show]
   end
-  resources :movies, only: %i[show]
 end
