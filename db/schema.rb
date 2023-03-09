a95692c6289459f7a54d44755a06556ea9698bff
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-09 12:50:41 +0800

    Adds index for Movie title to be unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_044047) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_044701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_044047) do
     t.decimal "rating", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_movies_on_title", unique: true
   end
 
   add_foreign_key "bookmarks", "lists"
