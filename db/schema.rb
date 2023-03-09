01574b6486b652217d314311f2326101e2762997
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-09 16:28:31 +0800

    Adds migration to add new column, image_url to List model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_053040) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_082657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_053040) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["name"], name: "index_lists_on_name", unique: true
   end
 
