24c7e9f29be48004f98fb68922ab4b6e01558c3d
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-10 22:21:35 +0800

    Adds seed to destroy the database before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@ require "open-uri"
 # Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 # Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
+List.destroy_all
+Movie.destroy_all
+Bookmark.destroy_all
+Review.destroy_all
+
 json_file = JSON.parse(URI.open("https://tmdb.lewagon.com/movie/top_rated").read)
 
 json_file["results"].each do |movie|
