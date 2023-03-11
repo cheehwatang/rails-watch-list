8423ae31908f1047579a887d912142d4f014085e
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-11 19:25:36 +0800

    Adds star rating to the new review form

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :list
 
   validates :content, presence: true, length: { minimum: 6 }
-  validates :rating,  presence: true, numericality: { only_integer: true, in: (0..5) }
+  validates :rating,  presence: true, numericality: { only_integer: true, in: (1..5) }
 end
