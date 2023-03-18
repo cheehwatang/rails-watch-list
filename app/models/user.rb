62b41b39dee34de18836a8c5fa31bc8724b96363
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-18 23:18:44 +0800

    Adds callbacks controller and routes and allow the user to process the request

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :lists, dependent: :destroy
 
   validates :username, presence: true
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+
+    user = User.where(email: data['email']).first
+    user ||= User.new(email: data['email'], password: Devise.friendly_token[0, 20], username: data['email'])
+    user.save
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   
   root 'lists#index'
   resources :lists, only: %i[index show new create] do
