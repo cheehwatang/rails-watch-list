eb84d60b9f8b7b23cf2775d5b466260503c01b9b
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-19 21:41:35 +0800

    Adds turbo to the review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,13 +4,16 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.list = @list
     
-    if @review.save
-      redirect_to list_path(@list), notice: 'Review was successfully posted'
-    else
-      @reviews = @list.reviews.order(updated_at: :DESC)
-      @bookmark = Bookmark.new
-      @movies = Movie.excluding(@list.movies).order(:title)
-      render 'lists/show', status: :unprocessable_entity
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to list_path(@list), notice: 'Review was successfully posted' }
+      else
+        @reviews = @list.reviews.order(updated_at: :DESC)
+        @bookmark = Bookmark.new
+        @movies = Movie.excluding(@list.movies).order(:title)
+        format.html { render 'lists/show', status: :unprocessable_entity }
+      end
+      format.json
     end
   end
 
